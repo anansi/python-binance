cfc75685624d22a3c5c7bdeff3f656b2e8a5a64a
Julian Hulme <julian@arcadesoftware.io>
2018-04-21 14:12:47 +0000

    udpate

---
+++ examples/test_order.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.insert(0, '../')
+sys.path.insert(0, '/root/sites/binance_python/')
+
+
+from datetime import datetime
+from binance.client import Client
+
+MY_KEY = "PXRZZR2Jv54oDVmTZcE10lXty2Swgj6tIXz5OqEGns5drO3bqlyLFkucD31dlnvt"
+MY_SECRET = "iBJEghWwUYL346Kny5uLCWC0j6UDmAtkbeeZjYHT1Zf9CiNmXWPcBLMTt75ekw1t"
+
+client = Client(MY_KEY, MY_SECRET) 
+order = client.create_order(
+    symbol='BTCUSD',
+    side=Client.SIDE_SELL,
+    type=Client.ORDER_TYPE_MARKET,
+    quantity=0.001)
+
+print "order placed"
