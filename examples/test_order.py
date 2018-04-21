56cc8ec1196111237e87476d5da29e78bf52496a
Julian Hulme <julian@arcadesoftware.io>
2018-04-21 14:23:06 +0000

    update

--- examples/test_order.py
+++ examples/test_order.py
@@ -9,9 +9,9 @@ from binance.client import Client
 MY_KEY = "PXRZZR2Jv54oDVmTZcE10lXty2Swgj6tIXz5OqEGns5drO3bqlyLFkucD31dlnvt"
 MY_SECRET = "iBJEghWwUYL346Kny5uLCWC0j6UDmAtkbeeZjYHT1Zf9CiNmXWPcBLMTt75ekw1t"
 
-client = Client(MY_KEY, MY_SECRET) 
+client = Client(MY_KEY, MY_SECRET)
 order = client.create_order(
-    symbol='BTCUSD',
+    symbol='BTCUSDT',
     side=Client.SIDE_SELL,
     type=Client.ORDER_TYPE_MARKET,
     quantity=0.001)
